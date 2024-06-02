bed689632ae6a0d7c87ae2cd81ec26b9d5704c7c
matteoNaN <matteonanni9897@gmail.com>
2024-06-02 19:55:23 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public void ReverseString(char[] s)
+        {
+            if (s.Length <= 1) return;
+
+            int m, n;
+            n = 0;
+            m = s.Length - 1;
+
+            while (n < m)
+            {
+                char temp = s[n];
+                s[n] = s[m];
+                s[m] = temp;
+                n++;
+                m--;
+            }
+        }
+
+    }
+}
